5e74adff4699d5e679c7e4bcc295e18a71e50b49
Josue Corea <coreaalirio@gmail.com>
2022-03-22 23:09:35 -0600

    Fix the form

--- index.php
+++ index.php
@@ -54,8 +54,8 @@
                 <div class="col-md-4 mb-3">
                     <div class="card text-center">
                       <div class="card-body">
-                        <h3 class="card-title text-capitalize"><?php $contact["name"]?></h3>
-                        <p class="m-2"><?php $contact["phone_number"]?></p>
+                        <h3 class="card-title text-capitalize"><?= $contact["name"]?></h3>          
+                        <p class="m-2"><?= $contact["phone_number"]?></p>
                         <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
                         <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
                       </div>
