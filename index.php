f9731a8cdc317ed70dd347583044725925091dd1
Josue Corea <coreaalirio@gmail.com>
2022-03-22 22:56:38 -0600

    Don't add contacts

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+  if(file_exists("contacts.json")){
+    $contacts=json_decode(file_get_contents("contacts.json"),true);
+  }else{
+    $contacts=[];
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,15 +13,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css" integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>Contacts App</title>
-    <link rel="stylesheet" href="/static/css/index.css">
+    <link rel="stylesheet" href="./static/css/index.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand font-weight-bold" href="#">
+          <a class="navbar-brand font-weight-bold" href="#">
             <img class="mr-2" src="./static/img/logo.png" />
             ContactsApp
           </a>
@@ -23,50 +30,40 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="./index.html">Home</a>
+                <a class="nav-link" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./add.html">Add Contact</a>
+                <a class="nav-link" href="./add.php">Add Contact</a>
               </li>
             </ul>
           </div>
         </div>
     </nav>
     <main>
-      <div class="container pt-5">
-        <div class="row justify-content-center">
-          <div class="col-md-8">
-            <div class="card">
-              <div class="card-header">Add New Contact</div>
-              <div class="card-body">
-                <form>
-                  <div class="mb-3 row">
-                    <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-  
-                    <div class="col-md-6">
-                      <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
-                    </div>
+        <div class="container pt-4 p-3">
+            <div class="row">
+              <?php if (count($contacts) == 0): ?>
+                <div class="col-md-4 mx-auto">
+                  <div class="card card-body text-center">
+                    <p>No contacts saved yet</p>
+                    <a href="add.php">Add One!</a>
                   </div>
-  
-                  <div class="mb-3 row">
-                    <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
-  
-                    <div class="col-md-6">
-                      <input id="phone_number" type="tel" class="form-control" name="phone_number" required autocomplete="phone_number" autofocus>
+                </div>
+              <?php endif ?>
+              <?php foreach ($contacts as $contact):?>
+                <div class="col-md-4 mb-3">
+                    <div class="card text-center">
+                      <div class="card-body">
+                        <h3 class="card-title text-capitalize"><?php $contact["name"]?></h3>
+                        <p class="m-2"><?php $contact["phone_number"]?></p>
+                        <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
+                        <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
+                      </div>
                     </div>
-                  </div>
-  
-                  <div class="mb-3 row">
-                    <div class="col-md-6 offset-md-4">
-                      <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
+                </div>
+              <?php endforeach?>
             </div>
-          </div>
         </div>
-      </div>
     </main>
 </body>
 </html>
